433e9bc9d396125125d8688c9b5517eb142f1576
jbellahou <56601030+jbellahou@users.noreply.github.com>
2023-09-06 09:48:00 -0500

    Create post.php

---
+++ post.php
@@ -0,0 +1,12 @@
+<?php
+$pageTitle = "Post page";
+include "view-header.php";
+?>
+    <h1>Post page</h1>
+<form method="post" action="post-result.php">
+  <input type="text" name="my-name">
+  <input type="submit" value="Post data">
+</form>
+<?php
+include "view-footer.php";
+?>
