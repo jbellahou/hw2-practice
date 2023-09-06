e7efb219553d71ac1a1c78ff002b21295dbfd27e
jbellahou <56601030+jbellahou@users.noreply.github.com>
2023-09-06 10:03:45 -0500

    Update get-result.php

--- get-result.php
+++ get-result.php
@@ -4,15 +4,13 @@ include "view-header.php";
 ?>
     <h1>Get result</h1>
 <?php
-if (isset($_GET['my-name'])) {
-  ?>
-  <p>The name sent is:</p>
-  <?php
-  echo $_GET['my-name'];
-} else {
-  ?>
-  <p>No data was sent to the page.</p>
-  <?php
-}
 include "view-footer.php";
+
+function getDisplay() {
+if (isset($_GET['my-name'])) {
+  return "<p>The name sent is:</p>" . $_GET['my-name'];
+} else {
+  return "<p>No data was sent to the page.</p>";
+}
+}
 ?>
